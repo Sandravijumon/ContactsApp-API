23d4573c7a4884219ca29307e59e569d7c4776fc
Sandravijumon <sandravijumon15@gmail.com>
2024-06-13 00:13:44 +0530

    created

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ app.post("/search",(req,res)=>{
     })
 })
 
-
+app.post("/delete",(req,res)=>{
+    let input=req.body
+    contactmodel.findByIdAndDelete(input._id).then(
+        (response)=>{res.json({status:"Success"})}
+    )
+})
 app.listen(8080,()=>{
     console.log("server started")
 })
