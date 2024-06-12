5ff003252b26a88f01002856541c01ea8ab0b1ee
Sandravijumon <sandravijumon15@gmail.com>
2024-06-12 23:35:41 +0530

    view created

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect("mongodb+srv://sandravijumon:sandra2001@cluster0.g6coklg.mongodb.net/contactDB?retryWrites=true&w=majority&appName=Cluster0")
 
 app.post("/add",(req,res)=>{
     let input=req.body
